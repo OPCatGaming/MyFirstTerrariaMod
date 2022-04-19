9695221ccfd6c2718b3b35b1b50eb2e7cd8757d8
OPCatGaming <joebear2007@gmail.com>
2022-04-19 12:52:30 -0400

    Squarehelper updated

--- Helpers/SquareHelper.cs
+++ Helpers/SquareHelper.cs
@@ -9,40 +9,74 @@ namespace Mymod.Helpers
 	{
 		public static void Helper(Player player, int heightInTiles, int widthInTiles, int tileid, bool mute, bool forced)
 		{
-			int UpperB = (heightInTiles * 16) / -2;
-			int LowB = (heightInTiles * 16) / 2;
-			int RightB = (widthInTiles * 16) / 2;
-			int LeftB = (widthInTiles * 16) / -2;
-			Vector2 URightC = new Vector2(RightB, UpperB);
-			Vector2 LRightC = new Vector2(RightB, LowB);
-			Vector2 ULeftC = new Vector2(LeftB, UpperB);
-			Vector2 LLeftC = new Vector2(LeftB, LowB);
+			//This stuff is for creating the bounds of the rectangle / square you are making and adjusting it depending on
+			//if it has an odd height or width
+			int UpperB = 0;
+			int LowB = 0;
+			int RightB = 0;
+			int LeftB = 0;
+			Vector2 URightC = new Vector2();
+			Vector2 LRightC = new Vector2();
+			Vector2 ULeftC = new Vector2();
+			Vector2 LLeftC = new Vector2();
 
-			//For the top blocks in the square
-			for (int UpperBL = (int)player.position.X + LeftB; UpperBL < (int)player.position.X + RightB; UpperBL += 16)
+			if (heightInTiles%2 == 0 && widthInTiles%2 == 0)
+            {
+				//Bounds (Integers)
+				UpperB = (heightInTiles * 16) / -2;
+				LowB = (heightInTiles * 16) / 2;
+			}
+			if (widthInTiles%2 == 0)
+            {
+				//Bounds (Integers)
+				RightB = (widthInTiles * 16) / 2;
+				LeftB = (widthInTiles * 16) / -2;
+			}
+			if (heightInTiles % 2 != 0)
 			{
-				Vector2 tileplace = new Vector2(player.position.X + UpperBL, player.position.Y + UpperB);
+				//Bounds (Integers)
+				UpperB = ((heightInTiles + 1) * 16) / -2;
+				LowB = ((heightInTiles + 1) * 16) / 2;
+				Main.NewText("This height of square is not acceptable yet!");
+			}
+			if (widthInTiles % 2 != 0)
+			{
+				//Bounds (Integers)
+				RightB = ((widthInTiles + 1) * 16) / 2;
+				LeftB = ((widthInTiles + 1) * 16) / -2;
+				Main.NewText("This width of square is not acceptable yet!");
+			}
+			URightC = new Vector2(player.position.X + (RightB - 16), player.position.Y + (UpperB + 16));
+			LRightC = new Vector2(player.position.X + (RightB - 16), player.position.Y + LowB);
+			ULeftC = new Vector2(player.position.X + LeftB, player.position.Y + (UpperB + 16));
+			LLeftC = new Vector2(player.position.X + LeftB, player.position.Y + LowB);
+
+			//These For loops are for creating the square
+			//For the top blocks in the square
+			for (float UpperBL = ULeftC.X; UpperBL < URightC.X; UpperBL += 16)
+			{
+				Vector2 tileplace = new Vector2(UpperBL, URightC.Y);
 				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, tileid, mute, forced, 1, 1);
 			}
 
 			//For the bottom blocks in the square
-			for (int LowerBL = (int)player.position.X + LeftB; LowerBL < (int)player.position.X + RightB; LowerBL += 16)
+			for (float LowerBL = ULeftC.X; LowerBL < URightC.X; LowerBL += 16)
 			{
-				Vector2 tileplace = new Vector2(player.position.X + LowerBL, player.position.Y + LowB);
+				Vector2 tileplace = new Vector2(LowerBL, LRightC.Y);
 				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, tileid, mute, forced, 1, 1);
 			}
 
 			//For the right blocks in the square
-			for (int RightBL = (int)player.position.X + LowB; RightBL < (int)player.position.X + UpperB; RightBL += 16)
+			for (float RightBL = LRightC.Y; RightBL < URightC.Y; RightBL += 16)
 			{
-				Vector2 tileplace = new Vector2(player.position.X + RightB, player.position.X + RightBL);
+				Vector2 tileplace = new Vector2(URightC.X, RightBL);
 				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, tileid, mute, forced, 1, 1);
 			}
 
 			//For the left blocks in the square
-			for (int LeftBL = (int)player.position.X + LowB; LeftBL < (int)player.position.X + UpperB; LeftBL += 16)
+			for (float LeftBL = LLeftC.Y; LeftBL < ULeftC.Y; LeftBL += 16)
 			{
-				Vector2 tileplace = new Vector2(player.position.X + LeftB, player.position.X + LeftBL);
+				Vector2 tileplace = new Vector2(ULeftC.X, LeftBL);
 				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, tileid, mute, forced, 1, 1);
 			}
 		}

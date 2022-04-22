1b3dc239fa9651c4f9731d45c5af2c498afab3d5
OPCatGaming <joebear2007@gmail.com>
2022-04-22 11:28:36 -0400

    Keybind add

---
+++ Common/Players/ExampleKeybindPlayer.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameInput;
+using Terraria.ModLoader;
+using Mymod.Common.Systems;
+
+namespace Mymod.Common.Players
+{
+	// See Common/Systems/KeybindSystem for keybind registration.
+	public class ExampleKeybindPlayer : ModPlayer
+	{
+		public override void ProcessTriggers(TriggersSet triggersSet)
+		{
+			if (KeybindSystem.RandomBuffKeybind.JustPressed)
+			{
+				int buff = Main.rand.Next(BuffID.Count);
+				Player.AddBuff(buff, 600);
+				Main.NewText($"ExampleMod's ModKeybind was just pressed. The {Lang.GetBuffName(buff)} buff was given to the player.");
+			}
+		}
+	}
+}

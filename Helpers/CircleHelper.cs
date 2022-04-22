269bf3f8fa203411e53c199cf3276734e7310b7a
OPCatGaming <joebear2007@gmail.com>
2022-04-21 20:15:08 -0400

    circle helper update

--- Helpers/CircleHelper.cs
+++ Helpers/CircleHelper.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Mymod.Helpers
 {
@@ -23,6 +25,23 @@ namespace Mymod.Helpers
 				}
 			}
 		}
+		public static List<Vector2> DrawCirclePointsLst(int points, double radius, Point center)
+		{
+			double slice = 2 * Math.PI / points;
+			List<Vector2> lst = new List<Vector2>();
+			for (int i = 0; i < points; i++)
+			{
+				double angle = slice * i;
+				int newX = (int)(center.X + radius * Math.Cos(angle));
+				int newY = (int)(center.Y + radius * Math.Sin(angle));
+				Point p = new Point(newX, newY);
+				lst.Add(new Vector2(newX, newY));
+				Vector2 tileplace = new Vector2(newX * 16, newY * 16);
+				Main.NewText($"{p} point location    {tileplace.ToTileCoordinates()} tile location");
+				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, TileID.Copper, false, true, 1, 1);
+			}
+			return lst;
+		}
 		public static void DrawCirclePoints(int points, double radius, Point center)
 		{
 			double slice = 2 * Math.PI / points;
@@ -37,6 +56,21 @@ namespace Mymod.Helpers
 				WorldGen.PlaceTile(tileplace.ToTileCoordinates().X, tileplace.ToTileCoordinates().Y, TileID.Copper, false, true, 1, 1);
 			}
 		}
+		public static async void gorehelper(int points, double radius, Point center)
+        {
+			for (int i = 1000; i > 1; i -= 10)
+            {
+				
+				for (int j = 0; j < points * 3; j++)
+                {
+					int dust = Dust.NewDust(center.ToVector2(), 1, 1, DustID.Silver);
+					Main.dust[dust].noGravity = true;
+					int index = Main.rand.Next(DrawCirclePointsLst(points, radius, center).Count);
+					Main.dust[dust].velocity = DrawCirclePointsLst(points, radius, center)[index] *= Main.rand.NextFloat();
+				}
+				await Task.Delay(i);
+			}
+        }
 	}
 }
 
